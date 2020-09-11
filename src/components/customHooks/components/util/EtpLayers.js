1ba2c52420ea112affee6fbaf3e611074a29da3b
YANG Wonjik <yang_wo@iMac-27-1.ad.iea.org>
2020-09-11 15:38:31 +0200

    added prop-types to break down controls

--- src/components/customHooks/components/util/EtpLayers.js
+++ src/components/customHooks/components/util/EtpLayers.js
@@ -23,126 +23,126 @@ export const ETP_LAYERS = [
         { url: "mapbox://iea.18yig51e", sourceLayer: "HDDCDD_5-boy2wn" }
       ],      
     },
-    {
-      data: 'HDD',
-      year: 2019,
-      type: 'SDS',
-      layers: [
-        { url: "mapbox://iea.d2w3aoj7", sourceLayer: "ETP_HDD_SDS_2019_1-5aiazc" },
-        { url: "mapbox://iea.7f0ea1pb", sourceLayer: "ETP_HDD_SDS_2019_2-blw0fl" },
-        { url: "mapbox://iea.dfyc5q9r", sourceLayer: "ETP_HDD_SDS_2019_3-b22gsd" }
-      ],
-    },
-    {
-      data: 'HDD',
-      year: 2030,
-      type: 'SDS',
-      layers: [
-        { url: "mapbox://iea.d2uckaci", sourceLayer: "ETP_HDD_SDS_2030_1-9ajsy2" },
-        { url: "mapbox://iea.7mb3qrxs", sourceLayer: "ETP_HDD_SDS_2030_2-05cl66" },
-        { url: "mapbox://iea.aidgwn4a", sourceLayer: "ETP_HDD_SDS_2030_3-1tsxi7" }
-      ],
-    },
-    {
-      data: 'HDD',
-      year: 2070,
-      type: 'SDS',
-      layers: [
-        { url: "mapbox://iea.bo74w66v", sourceLayer: "ETP_HDD_SDS_2070_1-8un57v" },
-        { url: "mapbox://iea.4796a4gs", sourceLayer: "ETP_HDD_SDS_2070_2-3m6xuc" },
-        { url: "mapbox://iea.3k7mr6cq", sourceLayer: "ETP_HDD_SDS_2070_3-6m852y" }
-      ],
-    },
-    {
-      data: 'HDD',
-      year: 2019,
-      type: 'STEPS',
-      layers: [
-        { url: "mapbox://iea.5g40uvj4", sourceLayer: "ETP_HDD_STEPS_2019_1-c1cn5h" },
-        { url: "mapbox://iea.9c9rfh2m", sourceLayer: "ETP_HDD_STEPS_2019_2-d568oa" },
-        { url: "mapbox://iea.ahrumdwk", sourceLayer: "ETP_HDD_STEPS_2019_3-azqdj8" }
-      ],
-    },
-    {
-      data: 'HDD',
-      year: 2030,
-      type: 'STEPS',
-      layers: [
-        { url: "mapbox://iea.9l8fbrh6", sourceLayer: "ETP_HDD_STEPS_2030_1-dscrj2" },
-        { url: "mapbox://iea.czn05k9t", sourceLayer: "ETP_HDD_STEPS_2030_2-7a9ede" },
-        { url: "mapbox://iea.dmlr02w0", sourceLayer: "ETP_HDD_STEPS_2030_3-4ycd0n" }
-      ],
-    },
-    {
-      data: 'HDD',
-      year: 2070,
-      type: 'STEPS',
-      layers: [
-        { url: "mapbox://iea.brulxm6r", sourceLayer: "ETP_HDD_STEPS_2070_1-a8vi7n" },
-        { url: "mapbox://iea.6qiqdhd5", sourceLayer: "ETP_HDD_STEPS_2070_2-1yjo5j" },
-        { url: "mapbox://iea.abszpwvq", sourceLayer: "ETP_HDD_STEPS_2070_3-9znrz1" }
-      ],
-    },
-    {
-      data: 'CDD',
-      year: 2019,
-      type: 'SDS',
-      layers: [
-        { url: "mapbox://iea.az6etx8j", sourceLayer: "ETP_CDD_SDS_2019_1-2byrtb" },
-        { url: "mapbox://iea.0vikjzk6", sourceLayer: "ETP_CDD_SDS_2019_2-8gk211" },
-        { url: "mapbox://iea.bz0p9ozh", sourceLayer: "ETP_CDD_SDS_2019_3-biq9cg" }
-      ],
-    },
-    {
-      data: 'CDD',
-      year: 2030,
-      type: 'SDS',
-      layers: [
-        { url: "mapbox://iea.d2uxozne", sourceLayer: "ETP_CDD_SDS_2030_1-a225js" },
-        { url: "mapbox://iea.5y2salzu", sourceLayer: "ETP_CDD_SDS_2030_2-b8quer" },
-        { url: "mapbox://iea.5mc9cssb", sourceLayer: "ETP_CDD_SDS_2030_3-7ekuf4" }
-      ],
-    },
-    {
-      data: 'CDD',
-      year: 2070,
-      type: 'SDS',
-      layers: [
-        { url: "mapbox://iea.9cptki9p", sourceLayer: "ETP_CDD_SDS_2070_1-7abetz" },
-        { url: "mapbox://iea.2i93f8fo", sourceLayer: "ETP_CDD_SDS_2070_2-dwzcgq" },
-        { url: "mapbox://iea.9f266pma", sourceLayer: "ETP_CDD_SDS_2070_3-9a7iou" }
-      ],
-    },
-    {
-      data: 'CDD',
-      year: 2019,
-      type: 'STEPS',
-      layers: [
-        { url: "mapbox://iea.a49it8vw", sourceLayer: "ETP_CDD_STEPS_2019_1-de5zex" },
-        { url: "mapbox://iea.12joncxi", sourceLayer: "ETP_CDD_STEPS_2019_2-1pyiac" },
-        { url: "mapbox://iea.7jl4uo6e", sourceLayer: "ETP_CDD_STEPS_2019_3-dyhl3r" }
-      ],
-    },
-    {
-      data: 'CDD',
-      year: 2030,
-      type: 'STEPS',
-      layers: [
-        { url: "mapbox://iea.7qa6v4n9", sourceLayer: "ETP_CDD_STEPS_2030_1-9ogcvu" },
-        { url: "mapbox://iea.d6w3pr3g", sourceLayer: "ETP_CDD_STEPS_2030_2-7kgb4j" },
-        { url: "mapbox://iea.cmvqbbl5", sourceLayer: "ETP_CDD_STEPS_2030_3-8l1gbx" }
-      ],
-    },
-    {
-      data: 'CDD',
-      year: 2070,
-      type: 'STEPS',
-      layers: [
-        { url: "mapbox://iea.blhli9op", sourceLayer: "ETP_CDD_STEPS_2070_1-6rfl8i" },
-        { url: "mapbox://iea.0r7rtf3n", sourceLayer: "ETP_CDD_STEPS_2070_2-6ulj7d" },
-        { url: "mapbox://iea.1rq9exi1", sourceLayer: "ETP_CDD_STEPS_2070_3-9gg4gg" }
-      ],
-    },
+    // {
+    //   data: 'HDD',
+    //   year: 2019,
+    //   type: 'SDS',
+    //   layers: [
+    //     { url: "mapbox://iea.d2w3aoj7", sourceLayer: "ETP_HDD_SDS_2019_1-5aiazc" },
+    //     { url: "mapbox://iea.7f0ea1pb", sourceLayer: "ETP_HDD_SDS_2019_2-blw0fl" },
+    //     { url: "mapbox://iea.dfyc5q9r", sourceLayer: "ETP_HDD_SDS_2019_3-b22gsd" }
+    //   ],
+    // },
+    // {
+    //   data: 'HDD',
+    //   year: 2030,
+    //   type: 'SDS',
+    //   layers: [
+    //     { url: "mapbox://iea.d2uckaci", sourceLayer: "ETP_HDD_SDS_2030_1-9ajsy2" },
+    //     { url: "mapbox://iea.7mb3qrxs", sourceLayer: "ETP_HDD_SDS_2030_2-05cl66" },
+    //     { url: "mapbox://iea.aidgwn4a", sourceLayer: "ETP_HDD_SDS_2030_3-1tsxi7" }
+    //   ],
+    // },
+    // {
+    //   data: 'HDD',
+    //   year: 2070,
+    //   type: 'SDS',
+    //   layers: [
+    //     { url: "mapbox://iea.bo74w66v", sourceLayer: "ETP_HDD_SDS_2070_1-8un57v" },
+    //     { url: "mapbox://iea.4796a4gs", sourceLayer: "ETP_HDD_SDS_2070_2-3m6xuc" },
+    //     { url: "mapbox://iea.3k7mr6cq", sourceLayer: "ETP_HDD_SDS_2070_3-6m852y" }
+    //   ],
+    // },
+    // {
+    //   data: 'HDD',
+    //   year: 2019,
+    //   type: 'STEPS',
+    //   layers: [
+    //     { url: "mapbox://iea.5g40uvj4", sourceLayer: "ETP_HDD_STEPS_2019_1-c1cn5h" },
+    //     { url: "mapbox://iea.9c9rfh2m", sourceLayer: "ETP_HDD_STEPS_2019_2-d568oa" },
+    //     { url: "mapbox://iea.ahrumdwk", sourceLayer: "ETP_HDD_STEPS_2019_3-azqdj8" }
+    //   ],
+    // },
+    // {
+    //   data: 'HDD',
+    //   year: 2030,
+    //   type: 'STEPS',
+    //   layers: [
+    //     { url: "mapbox://iea.9l8fbrh6", sourceLayer: "ETP_HDD_STEPS_2030_1-dscrj2" },
+    //     { url: "mapbox://iea.czn05k9t", sourceLayer: "ETP_HDD_STEPS_2030_2-7a9ede" },
+    //     { url: "mapbox://iea.dmlr02w0", sourceLayer: "ETP_HDD_STEPS_2030_3-4ycd0n" }
+    //   ],
+    // },
+    // {
+    //   data: 'HDD',
+    //   year: 2070,
+    //   type: 'STEPS',
+    //   layers: [
+    //     { url: "mapbox://iea.brulxm6r", sourceLayer: "ETP_HDD_STEPS_2070_1-a8vi7n" },
+    //     { url: "mapbox://iea.6qiqdhd5", sourceLayer: "ETP_HDD_STEPS_2070_2-1yjo5j" },
+    //     { url: "mapbox://iea.abszpwvq", sourceLayer: "ETP_HDD_STEPS_2070_3-9znrz1" }
+    //   ],
+    // },
+    // {
+    //   data: 'CDD',
+    //   year: 2019,
+    //   type: 'SDS',
+    //   layers: [
+    //     { url: "mapbox://iea.az6etx8j", sourceLayer: "ETP_CDD_SDS_2019_1-2byrtb" },
+    //     { url: "mapbox://iea.0vikjzk6", sourceLayer: "ETP_CDD_SDS_2019_2-8gk211" },
+    //     { url: "mapbox://iea.bz0p9ozh", sourceLayer: "ETP_CDD_SDS_2019_3-biq9cg" }
+    //   ],
+    // },
+    // {
+    //   data: 'CDD',
+    //   year: 2030,
+    //   type: 'SDS',
+    //   layers: [
+    //     { url: "mapbox://iea.d2uxozne", sourceLayer: "ETP_CDD_SDS_2030_1-a225js" },
+    //     { url: "mapbox://iea.5y2salzu", sourceLayer: "ETP_CDD_SDS_2030_2-b8quer" },
+    //     { url: "mapbox://iea.5mc9cssb", sourceLayer: "ETP_CDD_SDS_2030_3-7ekuf4" }
+    //   ],
+    // },
+    // {
+    //   data: 'CDD',
+    //   year: 2070,
+    //   type: 'SDS',
+    //   layers: [
+    //     { url: "mapbox://iea.9cptki9p", sourceLayer: "ETP_CDD_SDS_2070_1-7abetz" },
+    //     { url: "mapbox://iea.2i93f8fo", sourceLayer: "ETP_CDD_SDS_2070_2-dwzcgq" },
+    //     { url: "mapbox://iea.9f266pma", sourceLayer: "ETP_CDD_SDS_2070_3-9a7iou" }
+    //   ],
+    // },
+    // {
+    //   data: 'CDD',
+    //   year: 2019,
+    //   type: 'STEPS',
+    //   layers: [
+    //     { url: "mapbox://iea.a49it8vw", sourceLayer: "ETP_CDD_STEPS_2019_1-de5zex" },
+    //     { url: "mapbox://iea.12joncxi", sourceLayer: "ETP_CDD_STEPS_2019_2-1pyiac" },
+    //     { url: "mapbox://iea.7jl4uo6e", sourceLayer: "ETP_CDD_STEPS_2019_3-dyhl3r" }
+    //   ],
+    // },
+    // {
+    //   data: 'CDD',
+    //   year: 2030,
+    //   type: 'STEPS',
+    //   layers: [
+    //     { url: "mapbox://iea.7qa6v4n9", sourceLayer: "ETP_CDD_STEPS_2030_1-9ogcvu" },
+    //     { url: "mapbox://iea.d6w3pr3g", sourceLayer: "ETP_CDD_STEPS_2030_2-7kgb4j" },
+    //     { url: "mapbox://iea.cmvqbbl5", sourceLayer: "ETP_CDD_STEPS_2030_3-8l1gbx" }
+    //   ],
+    // },
+    // {
+    //   data: 'CDD',
+    //   year: 2070,
+    //   type: 'STEPS',
+    //   layers: [
+    //     { url: "mapbox://iea.blhli9op", sourceLayer: "ETP_CDD_STEPS_2070_1-6rfl8i" },
+    //     { url: "mapbox://iea.0r7rtf3n", sourceLayer: "ETP_CDD_STEPS_2070_2-6ulj7d" },
+    //     { url: "mapbox://iea.1rq9exi1", sourceLayer: "ETP_CDD_STEPS_2070_3-9gg4gg" }
+    //   ],
+    // },
     {
       data: 'NFC',
       year: 2019,
